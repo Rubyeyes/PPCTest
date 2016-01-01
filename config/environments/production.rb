fb9763ed30fb915d627868bfe15929b4c481a1b7
Rubyeyes <catian315@gmail.com>
2015-12-31 19:36:01 -0700

    Sign up confirm email for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,13 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Config sign up confirm email
+  config.action_mailer.default_url_options = {:host => 'https://limitless-wildwood-4680.herokuapp.com'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  :address => "127.0.0.1",
+  :port    => 25,
+  :domain  => 'https://limitless-wildwood-4680.herokuapp.com'
+}
 end
