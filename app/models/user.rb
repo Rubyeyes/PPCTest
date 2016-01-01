ec5053dbb1b67ed28487275cefda0eda0c7da4ec
Rubyeyes <catian315@gmail.com>
2015-12-31 22:01:11 -0700

    Add role to user db

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,20 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
-         :recoverable, :rememberable, :trackable, :validatable     
+         :recoverable, :rememberable, :trackable, :validatable   
+
+  # User::Roles
+  # The available roles
+  Roles = [ :admin , :default ]
+
+  def is?( requested_role )
+    self.role == requested_role.to_s
+  end  
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :current_password, :password, :password_confirmation, :role)
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151231073521) do
+ActiveRecord::Schema.define(version: 20160101045830) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20151231073521) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
+    t.string   "role"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
