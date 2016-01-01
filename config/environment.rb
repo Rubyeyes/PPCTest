cde9dbced0712be1194d5666cd6665badd4045d3
Rubyeyes <catian315@gmail.com>
2015-12-31 20:53:49 -0700

    Modify sandgrid config again

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,14 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  :user_name => "yourSendGridusernameyougetfromheroku",
+  :password => "yourSendGridpasswordyougetfromheroku",
+  :domain => "staging.freelanceful.com",
+  :address => "smtp.sendgrid.net",
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,14 +81,4 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = {:host => 'https://limitless-wildwood-4680.herokuapp.com'}
-  config.action_mailer.delivery_method = :smtp
-  ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name      => ENV['app45643249@heroku.com'],
-    :password       => ENV['uwb5a5lr1393'],
-    :domain         => 'heroku.com',
-    :enable_starttls_auto => true
-  }
 end
