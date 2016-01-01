4ee8dacf2b133ef6659a96722fa1947ceceb76bd
Rubyeyes <catian315@gmail.com>
2015-12-31 21:14:53 -0700

    Modify sandgrid config 5

--- config/environment.rb
+++ config/environment.rb
@@ -9,8 +9,8 @@ ActionMailer::Base.smtp_settings = {
 :address        => 'smtp.sendgrid.net',
 :port           => '587',
 :authentication => :plain,
-:user_name      => ENV['app45643249@heroku.com'],
-:password       => ENV['uwb5a5lr1393'],
+:user_name      => ENV['SENDGRID_USERNAME'],
+:password       => ENV['SENDGRID_PASSWORD'],
 :domain         => 'heroku.com',
 :enable_starttls_auto => true
 }
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'https://limitless-wildwood-4680.herokuapp.com'
+  config.mailer_sender = 'app45643249@heroku.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
