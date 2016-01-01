647ffaf08805daeae7c32ce2e46b84e0740ef9ee
Rubyeyes <catian315@gmail.com>
2015-12-31 21:03:19 -0700

    Modify sandgrid config 4

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'PPCTestAccount@example.com'
+  config.mailer_sender = 'https://limitless-wildwood-4680.herokuapp.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
