a21e98b674c2b06f7117ee05360dfb0bfac012fc
Rubyeyes <catian315@gmail.com>
2016-01-28 15:42:57 -0800

    Add some user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,10 +5,26 @@ module ApplicationHelper
 	end
 
 	def smart_navbar_color controller
-		if controller == 'home' || ('devise/sessions' && action_name == 'new')
+		if controller == 'home' || (controller == 'devise/sessions' && action_name == 'new')
 			"navbar-full navbar-inverse navbar-transparent"
 		else
 			"navbar-full navbar-inverse navbar-normal"
+		end
+	end
+
+	def smart_navbar_position controller
+		if controller == 'home' || (controller == 'devise/sessions' && action_name == 'new')
+			"navbar-static-top"
+		else
+			"navbar-fixed-top"
+		end
+	end
+
+	def smart_container controller
+		if controller == 'home' || (controller == 'devise/sessions' && action_name == 'new')
+			"container-fluid"
+		else
+			"container"
 		end
 	end
 	
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -14,6 +14,7 @@ class Ability
       can :manage, Cost
       can [:read], Project
       can [:read], Product
+      can [:read, :edit, :mamage, :update], Notification
     else
       cannot [:mamage, :read], :all
     end
