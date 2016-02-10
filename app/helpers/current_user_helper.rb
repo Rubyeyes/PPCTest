5a93c6b47612be67a5e618084625702908e2544b
Rubyeyes <catian315@gmail.com>
2016-02-10 14:59:59 -0800

    Add sort to task

--- app/helpers/current_user_helper.rb
+++ app/helpers/current_user_helper.rb
@@ -1,2 +1,7 @@
 module CurrentUserHelper
+	def sortable column, title = nil
+		title ||=colunm.titleize
+		direction = column == params[:sort] && params[:direction] == "asc" ? "desc" : "asc"
+		link_to title, current_user_index_path(params.merge(sort: column, direction: direction))
+	end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -3,4 +3,12 @@ class Task < ActiveRecord::Base
 	belongs_to :executor, class_name: 'User', foreign_key: 'executor_id'
 	has_many :notifications, as: :notifiable, dependent: :delete_all
 
+	def self.text_sort sort, direction
+		if sort.present?
+			joins(:project).order("#{sort} #{direction}")
+		else
+			joins(:project).order(' deadline DESC, fullname ASC')
+		end		
+	end
+
 end
